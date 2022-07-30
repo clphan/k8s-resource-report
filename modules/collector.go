a4122639336c333a0419a0776885389ff699b0a9
clphan <pdkhanh06@gmail.com>
2022-07-30 14:04:22 +0700

    Test rest api client-go for metrics

--- modules/collector.go
+++ modules/collector.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"flag"
+	"fmt"
 	"path/filepath"
 	"time"
 
@@ -69,6 +70,7 @@ func GetMetricClientApi(clientset *kubernetes.Clientset, pods *PodMetricsList) e
 	if err != nil {
 		panic(err.Error())
 	}
+	fmt.Println(json.Marshal(data))
 	err = json.Unmarshal(data, &pods)
 	return err
 }
